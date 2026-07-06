6226712e32219e7659a1a1d938043e3cf8cde849
noeg18 <nagat_g@hotmail.com>
2026-07-06 09:22:36 +0100

    Add functionality in economics app

--- R_&_shiny_practice/shiny_practice/economics_app/app.R
+++ R_&_shiny_practice/shiny_practice/economics_app/app.R
@@ -2,33 +2,76 @@ library(shiny)
 library(ggplot2)
 library(bslib)
 
-y <- c("pce", "psavert", "uempmed")
-x <- c("pop", "unemploy") 
+x <- c("Personal Consumption Expenditures" = "pce", "Personal Savings Rate" = "psavert", "Median Duration of Unemployment" = "uempmed")
+xlabel <- c(pce = "Personal Consumption Expenditures, in billions of dollars", psavert= "Personal Savings Rate", uempmed = "Median Duration of Unemployment, in weeks")
+y <- c("Total Population" = "pop", "Number of Unemployed" = "unemploy")
+ylabel <- c(pop = "Total Population, in thousands", unemploy = "Number of Unemployed, in thousands")
+
 
 ui <- fluidPage(
     theme = bs_theme(
-        fg = "#DFB793", bg = "#904122"
+        fg = "#925f94", bg = "#f3f3f3"
     ),
-    selectInput("x", label = "x-axis", choices = x, selected = "pce"),
-    selectInput("y", label = "y-axis", choices = y, selected = "pop"),
-    plotOutput("plot")
-)
+    selectInput("x", label = "Select x-axis", choices = x, selected = "Personal Consumption Expenditures"),
+    selectInput("y", label = "Select y-axis", choices = y, selected = "Total Population"),
+    plotOutput("plot", hover = hoverOpts("plot_hover", delay = 30, delayType = "debounce")),
+    uiOutput("hover_box")
+    )
 
 server <- function(input, output, session){
-    bs_themer()
+    #bs_themer()
     output$plot <- renderPlot({
-        ggplot(economics, aes(x = .data[[input$x]], y= .data[[input$y]])) +
-        geom_point() +
-        labs(title = "US Economic Time Series", x = input$x, y = input$y) +
+        ggplot(economics, aes(x = .data[[input$x]], y= .data[[input$y]], color = date)) +
+        geom_point(size = 2) +
+        labs(title = "US Economic Time Series", x = xlabel[[input$x]], y = ylabel[[input$y]]) +
+        scale_y_continuous(labels = scales::comma) +
+        scale_x_continuous(labels = scales::comma) +
         theme_minimal() +
         theme(
-            plot.background = element_rect(fill = "#904122", color = NA),
-            panel.background = element_rect(fill = "#af5f40", color = NA),
-            plot.title = element_text(face = "bold", size = 36, color = "#DFB793", family = "mono"),
-            axis.title = element_text(face = "bold", size = 18, color = "#DFB793"),
-            axis.text = element_text(color = "#DFB793", size = 10)
-        )
+            plot.background = element_rect(fill = "#f3f3f3", color = NA),
+            panel.background = element_rect(fill = "#f3f3f3", color = NA),
+            plot.title = element_text(size = 36, color = "#925f94", family = "Calibri"),
+            axis.title = element_text(size = 18, color = "#925f94", family = "Calibri"),
+            axis.text = element_text(color = "black", size = 14, family = "Calibri")
+        ) 
     })
+    output$hover_box <- renderUI({
+        if(is.null(input$plot_hover)){
+            return(NULL)
+        }
+        point <- nearPoints(
+            economics,
+            input$plot_hover,
+            xvar = input$x,
+            yvar = input$y,
+            maxpoints = 1
+        )
+        if(nrow(point) == 0){
+            return(NULL)
+        }
+        dynamic_x <- input$x
+        x_value <- point[[dynamic_x]]
+        dynamic_y <- input$y
+        y_value <- point[[dynamic_y]]
+        
+        left_px <- input$plot_hover$coords_css$x + 10
+        top_px <- input$plot_hover$coords_css$y + 10
+
+        absolutePanel(
+            left = left_px,
+            top = top_px,
+            style = "background: #f3f3f3; font-size:16px; height: 80px;",
+            tags$pre(
+                paste0(
+                    "Date: ", as.character(point$date), "\n",
+                    names(x)[x == input$x], ": ", x_value, "\n",
+                    names(y)[y == input$y], ": ", y_value, "\n"
+            )
+            )
+        )
+        
+    })
+
 }
 
 shinyApp(ui, server)
